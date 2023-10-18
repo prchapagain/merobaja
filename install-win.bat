e7dc7290a9ac4dd65bd75bf5a92accf728815560
khumnath <nath.khum@gmail.com>
2023-10-19 07:17:56 +0900

    remove unnecessary command

--- install-win.bat
+++ install-win.bat
@@ -6,5 +6,4 @@ cd "merobaja"
 xcopy  /s /e /y "windows" "C:\Program Files\Steinberg\Vstplugins"
 cd ".."
 rmdir /Q /S merobaja
-del install-win.bat
 echo "plugin installed and cleared temporary files!! now ask us for activation key"
